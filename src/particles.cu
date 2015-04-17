ab1dc1f231c5d5a10fab380138cb75eba412d3c4
Jack Browne <jackdbrowne@googlemail.com>
2015-04-17 16:41:42 +0100

    Sort of fixed stuff!

--- src/particles.cu
+++ src/particles.cu
@@ -16,13 +16,10 @@ static int gridSize;
 static int blockSize;
 static Particle *d_particles;
 static Particle *d_particles_out;
-static int *d_particleIdxs;
+// static int *d_particleIdxs;
 static unsigned int *mortonCodes;
 static unsigned int *d_morton_codes;
 static DeviceVec *d_forces;
-static thrust::host_vector<BVHNode> nodes;
-static BVHNode *d_nodes;
-static int numBVHNodes;
 
 /**
  *  3D vector class for the device.
@@ -119,8 +116,9 @@ public:
  * Hierarchy structure.
  */
 class AABB {
-private:
+public:
 	DeviceVec centre;
+// private:
 	float width;
 	float height;
 	float depth;
@@ -173,17 +171,17 @@ public:
 		float halfDepth = depth / 2;
 		float oHalfDepth = other->depth / 2;
 
-		if (getRight(halfWidth) <= other->getLeft(oHalfWidth))
+		if (getRight(halfWidth) < other->getLeft(oHalfWidth))
 			return false;
-		if (getLeft(halfWidth) >= other->getRight(oHalfWidth))
+		if (getLeft(halfWidth) > other->getRight(oHalfWidth))
 			return false;
-		if (getBottom(halfHeight) >= other->getTop(oHalfHeight))
+		if (getBottom(halfHeight) > other->getTop(oHalfHeight))
 			return false;
-		if (getTop(halfHeight) <= other->getBottom(oHalfHeight))
+		if (getTop(halfHeight) < other->getBottom(oHalfHeight))
 			return false;
-		if (getFront(halfDepth) <= other->getBack(oHalfDepth))
+		if (getFront(halfDepth) < other->getBack(oHalfDepth))
 			return false;
-		if (getBack(halfDepth) >= other->getFront(oHalfDepth))
+		if (getBack(halfDepth) > other->getFront(oHalfDepth))
 			return false;
 
 		return true;
@@ -220,27 +218,27 @@ public:
  * tree of AABBs.
  */
 struct BVHNode {
-	Particle *particle;
+	int particleIdx;
 	AABB boundingBox;
 	int leftChildIdx, rightChildIdx;
 
 	BVHNode() :
-		particle(NULL), leftChildIdx(-1), rightChildIdx(-1)
+		particleIdx(-1), boundingBox(), leftChildIdx(-1), rightChildIdx(-1)
 	{ }
 
 	// Constructor creates an internal (non-leaf) node
 	BVHNode(AABB aabb, int l, int r) :
-		particle(NULL), boundingBox(aabb), leftChildIdx(l), rightChildIdx(r)
+		particleIdx(-1), boundingBox(aabb), leftChildIdx(l), rightChildIdx(r)
 	{ }
 
-	static BVHNode leafNode(Particle *p) {
+	static BVHNode leafNode(Particle *p, int idx) {
 		BVHNode node(AABB::fromParticle(p), -1, -1);
-		node.particle = p;
+		node.particleIdx = idx;
 
 		return node;
 	}
 
-	__device__ bool isLeaf() {
+	__host__ __device__ bool isLeaf() {
 		return leftChildIdx == -1 && rightChildIdx == -1;
 	}
 
@@ -253,7 +251,17 @@ struct BVHNode {
 	}
 };
 
+static thrust::host_vector<BVHNode> nodes;
+static BVHNode *d_nodes;
+static int numBVHNodes;
+
 __device__ bool particlesCollide(Particle *p1, Particle *p2) {
+	// #if __CUDA_ARCH__ >= 200
+	// 	printf("particlesCollide() called!\n");
+	// 	printf("p1.position.x: %.4f,p1.position.y: %.4f, p1.position.z: %.4f\n", p1->position.x, p1->position.y, p1->position.z);
+	// 	printf("p2.position.x: %.4f,p2.position.y: %.4f, p2.position.z: %.4f\n", p2->position.x, p2->position.y, p2->position.z);
+	// #endif
+
 	DeviceVec collideVec = DeviceVec(&(p2->position))
 			- DeviceVec(&(p1->position));
 
@@ -278,30 +286,74 @@ __device__ void collide(Particle *p1, Particle *p2, DeviceVec *force) {
 	*force = *force + norm * -0.5 * (collideDist - dist);
 }
 
-__device__ void traverse( BVHNode node, BVHNode *nodes, AABB& queryAABB, Particle* particle, int particleIdx, DeviceVec* forces )
+__device__ void traverse( BVHNode node, BVHNode *nodes, AABB& queryAABB, Particle *particles, Particle* particle, int particleIdx, DeviceVec* forces )
 {
 	// Bounding box overlaps the query => process node.
 	if (node.boundingBox.intersects(&queryAABB))
 	{
 		// Leaf node => resolve collision.
 		if (node.isLeaf()) {
-			if(particlesCollide(particle, node.particle))
-				collide(particle, node.particle, &forces[particleIdx]);
+			if(particleIdx != node.particleIdx){
+				if(particlesCollide(particle, &particles[node.particleIdx])){
+					collide(particle, &particles[node.particleIdx], &forces[particleIdx]);
+
+					// #if __CUDA_ARCH__ >= 200
+					// 	if (particleIdx == 2){
+					// 		printf("particlesCollide(particle, node.particle)\n");
+					// 	}
+					// #endif
+				}
+			}
+
+			// #if __CUDA_ARCH__ >= 200
+			// if (particleIdx == 2){
+			// 	printf("[x: %.4f, y: %.4f, z: %.4f] ", node.boundingBox.centre.x, node.boundingBox.centre.y, node.boundingBox.centre.z);
+			// 	printf("Call finished.\n");
+			// }
+			// #endif
 		}
 
 		// Internal node => recurse to children.
 		else
 		{
 			if (node.hasLeftChild()) {
+				// #if __CUDA_ARCH__ >= 200
+				// if (particleIdx == 2){
+				// 	printf("[x: %.4f, y: %.4f, z: %.4f] ", node.boundingBox.centre.x, node.boundingBox.centre.y, node.boundingBox.centre.z);
+				// 	printf("Traversing left child: #%d \n", node.leftChildIdx);
+				// }
+				// #endif
 				BVHNode childL = nodes[node.leftChildIdx];
-				traverse(childL, nodes, queryAABB, particle, particleIdx, forces);
+				traverse(childL, nodes, queryAABB, particles, particle, particleIdx, forces);
+				// #if __CUDA_ARCH__ >= 200
+				// if (particleIdx == 2)
+				// 	printf("[x: %.4f, y: %.4f, z: %.4f] ", node.boundingBox.centre.x, node.boundingBox.centre.y, node.boundingBox.centre.z);
+				// 	printf("Left child traversed.\n");
+				// #endif
 			}
 			if (node.hasRightChild()) {
+				// #if __CUDA_ARCH__ >= 200
+				// if (particleIdx == 2){
+				// 	printf("[x: %.4f, y: %.4f, z: %.4f] ", node.boundingBox.centre.x, node.boundingBox.centre.y, node.boundingBox.centre.z);
+				// 	printf("Traversing right child: #%d \n", node.rightChildIdx);
+				// }
+				// #endif
 				BVHNode childR = nodes[node.rightChildIdx];
-				traverse(childR, nodes, queryAABB, particle, particleIdx, forces);
+				traverse(childR, nodes, queryAABB, particles, particle, particleIdx, forces);
+				// #if __CUDA_ARCH__ >= 200
+				// if (particleIdx == 2)
+				// 	printf("[x: %.4f, y: %.4f, z: %.4f] ", node.boundingBox.centre.x, node.boundingBox.centre.y, node.boundingBox.centre.z);
+				// 	printf("Right child traversed.\n");
+				// #endif
 			}
 		}
 	}
+	// #if __CUDA_ARCH__ >= 200
+	// if (particleIdx == 2){
+	// 	printf("[x: %.4f, y: %.4f, z: %.4f] ", node.boundingBox.centre.x, node.boundingBox.centre.y, node.boundingBox.centre.z);
+	// 	printf("Call finished.\n");
+	// }
+	// #endif
 }
 
 __global__ void moveParticles(int bvhRootIdx, BVHNode *nodes, Particle *particles, Particle *out, int size, DeviceVec *forces) {
@@ -320,25 +372,30 @@ __global__ void moveParticles(int bvhRootIdx, BVHNode *nodes, Particle *particle
 		forces[in_x] = DeviceVec(0, 0, 0);
 
 		AABB query = AABB::fromParticle(&thisParticle);
-		traverse(nodes[bvhRootIdx], nodes, query, &thisParticle, in_x, forces);
+		traverse(nodes[bvhRootIdx], nodes, query, particles, &thisParticle, in_x, forces);
 
+		// #if __CUDA_ARCH__ >= 200
+		// 	// printf("x: %f.4, y: %f.4, z: %f.4", forces[in_x].x, forces[in_x].y, forces[in_x].z);
+		// 	printf("Traversed!\n");
+		// #endif
+		
 		__syncthreads();
 
 		velD = velD + forces[in_x];
 
-//		DeviceVec force(0, 0, 0);
-//
-//		for (int i = 0; i < size; i++) {
-//			if (i != in_x) { // Don't consider ourselves
-//				Particle other = particles[i];
-//
-//				if (particlesCollide(&thisParticle, &other)) {
-//					collide(&thisParticle, &other, &force);
-//				}
-//			}
-//		}
-//
-//		velD = velD + force;
+		// DeviceVec force(0, 0, 0);
+
+		// for (int i = 0; i < size; i++) {
+		// 	if (i != in_x) { // Don't consider ourselves
+		// 		Particle other = particles[i];
+
+		// 		if (particlesCollide(&thisParticle, &other)) {
+		// 			collide(&thisParticle, &other, force);
+		// 		}
+		// 	}
+		// }
+
+		// velD = velD + force;
 
 		// Calculate our new desired position
 		newPosD = newPosD + velD;
@@ -523,7 +580,7 @@ BVHNode generateBVH(unsigned int *sortedMortonCodes, Particle *particles, int *s
 
 	// Base case: create a leaf node
 	if (first == last) {
-		BVHNode node = BVHNode::leafNode(&particles[sortedParticleIdxs[first]]);
+		BVHNode node = BVHNode::leafNode(&particles[sortedParticleIdxs[first]], sortedParticleIdxs[first]);
 
 		nodes.push_back(node);
 		return node;
@@ -561,7 +618,7 @@ void cuda_init(int numParticles) {
 	cudaMalloc((void**) &d_particles, sizeof(Particle) * numParticles);
 	cudaMalloc((void**) &d_particles_out, sizeof(Particle) * numParticles);
 	cudaMalloc((void**) &d_forces, sizeof(DeviceVec) * numParticles);
-	cudaMalloc((void**) &d_particleIdxs, sizeof(int) * numParticles);
+	// cudaMalloc((void**) &d_particleIdxs, sizeof(int) * numParticles);
 	cudaMalloc((void**) &d_morton_codes, sizeof(unsigned int) * numParticles);
 
 	cudaError_t err = cudaGetLastError();
@@ -571,7 +628,7 @@ void cuda_init(int numParticles) {
 	computeGridSize(numParticles, 256, gridSize, blockSize);
 }
 
-//static int count = 0;
+// static int count = 0;
 
 void particles_update(Particle *particles, int particlesSize) {
 	// copy host memory to device
@@ -608,7 +665,17 @@ void particles_update(Particle *particles, int particlesSize) {
 
 	// Generate the BVH
 	numBVHNodes = 0;
+	nodes.clear();
 	BVHNode rootNode = generateBVH(mortonCodes, particles, particleIdxs, 0, particlesSize - 1, numBVHNodes, nodes);
+
+	// printf("id_x: %.4f, id_y: %.4f, idz_: %.4f\n", rootNode.boundingBox.centre.x, rootNode.boundingBox.centre.y, rootNode.boundingBox.centre.z);
+	// for (int i = 0; i < numBVHNodes; ++i)
+	// {
+	// 	printf("[%d]\tx: %.4f, y: %.4f, z: %.4f\n", i, nodes[i].boundingBox.centre.x, nodes[i].boundingBox.centre.y, nodes[i].boundingBox.centre.z);
+	// 	printf("\twidth: %.4f, height: %.4f, depth: %.4f\n", i, nodes[i].boundingBox.width, nodes[i].boundingBox.height, nodes[i].boundingBox.depth);
+	// 	printf("\t\tleftChildIdx: %d, rightChildIdx: %d\n", nodes[i].leftChildIdx, nodes[i].rightChildIdx);
+	// 	printf("\t\tisLeaf(): %d\n", nodes[i].isLeaf());
+	// }
 
 //	printf("Generated.\n");
 
@@ -640,9 +707,9 @@ void particles_update(Particle *particles, int particlesSize) {
 
 	cudaThreadSynchronize();
 
-//	err = cudaGetLastError();
-//	if (err != cudaSuccess)
-//	    printf("[%d] Move Particles Error: %s\n", count++, cudaGetErrorString(err));
+	// err = cudaGetLastError();
+	// if (err != cudaSuccess)
+	//     printf("[%d] Move Particles Error: %s\n", count++, cudaGetErrorString(err));
 
 	// copy result from device to host
 	cudaMemcpy(particles, d_particles_out, sizeof(Particle) * particlesSize, cudaMemcpyDeviceToHost);
